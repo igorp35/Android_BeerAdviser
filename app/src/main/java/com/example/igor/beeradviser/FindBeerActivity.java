185b19bc484b99cd5df7d89f71fca08968909615
igorp35 <igorp35@gmail.com>
2016-08-24 08:23:38 +0400

    Commit-CreatButton

--- app/src/main/java/com/example/igor/beeradviser/FindBeerActivity.java
+++ app/src/main/java/com/example/igor/beeradviser/FindBeerActivity.java
@@ -1,5 +1,6 @@
 package com.example.igor.beeradviser;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,26 +9,45 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class FindBeerActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_beer);
-      //  Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-      //   setSupportActionBar(toolbar);
+        //  Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //   setSupportActionBar(toolbar);
 
-       //  FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-       // fab.setOnClickListener(new View.OnClickListener() {
-       //     @Overrid
-       //    public void onClick(View view) {
-       //      Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-       //            .setAction("Action", null).show();
-       // }
-       // });
+        //  FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        // fab.setOnClickListener(new View.OnClickListener() {
+        //     @Overrid
+        //    public void onClick(View view) {
+        //      Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+        //            .setAction("Action", null).show();
+        // }
+        // });
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
     }
 
+    public void onClickFindBeer(View view)
+    {
+        Spinner spinner_color = (Spinner) findViewById(R.id.spinner_color);
+        TextView textView_brand = (TextView) findViewById(R.id.textView_brand);
+
+        String beerType = String.valueOf(spinner_color.getSelectedItem());
+        textView_brand.setText(beerType);
+
+    }
+    //==============================================================================================//
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -49,4 +69,6 @@ public class FindBeerActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
